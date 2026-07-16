6494dd3c1c714031c91be594558648639906cda5
Rudransh Chouksey <choukseyrudransh22@gmail.com>
2026-07-16 12:09:12 +0530

    Deploy changes

--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -2,7 +2,8 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: import.meta.env.VITE_GRAPHQL_URL || 'http://localhost:4000/graphql',
+  // Dynamically uses Vite's environment variable injected at build/runtime
+  uri: import.meta.env.VITE_GRAPHQL_API_URL || 'http://localhost:4000/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
